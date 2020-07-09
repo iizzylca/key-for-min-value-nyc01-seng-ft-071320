75c8e80fe880858e1a9a57bcf2ead57cb23c8a04
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 08:15:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     if min_value == nil
       #binding.pry
       min_value = value
-      min_key = name
+      min_name = name
     elsif min_value > value
       #binding.pry
       min_value = value
