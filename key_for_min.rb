ecf17eca72c9e137a5ef7d0ad2b9de319ebdcfc7
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 08:31:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
 
   name_hash.each do |key,value|
+    binding.pry
     #if the lowest_value is in it's intial state
     #or if the value of the current key is less than the lowest_value
     #then: set the lowest_value to value and lowest_key to key
